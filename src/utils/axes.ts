7f0ce75807e35f175ee07c7f7f45e4c88417718c
yavchris07 <wilondjayav27@gmail.com>
2026-08-28 11:25:40 +0200

    Refactor : Axex prioritaire

--- src/utils/axes.ts
+++ src/utils/axes.ts
@@ -1,48 +1,40 @@
 export const axes = [
   {
     id: 1,
-    name: "Gouvernance responsable, transparente et durable de régimes fonciers, peches et aspects connexes",
+    name: "Gouvernance responsable, transparente et durable de régimes fonciers, pêches et aspects connexes",
   },
   {
     id: 2,
-    name: "Appuis aux réformes juridiques et institutionnelles impactant les fonciers, énergies renouvelables et l&apos;environnement.",
+    name: "Appuis aux réformes juridiques et institutionnelles impactant les fonciers, énergies renouvelables et l'environnement.",
   },
   {
     id: 3,
-    name: "Justice environnementale et protection de la faune et flore sauvages par l&apos;aide légale ",
+    name: "Justice environnementale et protection de la faune et flore sauvages par l'aide légale ",
   },
   {
     id: 4,
-    name: "Protections légale de défenseurs des droites fonciers et l&apos;environnement",
+    name: "Protections légale de défenseurs des droites fonciers et l'environnement",
   },
 
   {
     id: 5,
-    name: "Accès à tous à l&apos;énergie durable, abordable et fiable pro pauvres",
+    name: "Accès à tous à l'énergie durable, abordable et fiable pro pauvres",
   },
   {
     id: 6,
     name: "Conentieux climatiques stratégiques, appui juridique légale aux investissements locaux verts pour une transition juste",
-  },
-  {
-    id: 7,
-    name: "Domaine Foncier",
-  },
-  {
-    id: 8,
-    name: "Energie",
-  },
-
-  {
-    id: 9,
-    name: "Ressources naturelles et Environnement",
-  },
-  {
-    id: 10,
-    name: "Protection des DDHE et DDF",
-  },
-  {
-    id: 11,
-    name: "Application des lois pour la faune et la flore sauvages",
-  },
+  }
 ];
+
+    // • Gouvernance responsable, transparente et durable des régimes fonciers, pêches et aspects connexes,
+
+    // • Appui aux réformes juridiques et institutionnelles impactant les fonciers, énergies renouvelables et l’environnement,
+
+
+    // • Justice environnementale et protection de la faune et flore sauvages par l’aide légale
+
+    // • Protection légale des défenseurs des droits fonciers et de l’environnement,
+
+    // • Accès pour tous à l’énergie durable, abordable et fiable aux tarifs pro pauvres,
+
+    // • Contentieux climatiques stratégiques, appui légal aux investissements locaux verts pour une transition juste.
